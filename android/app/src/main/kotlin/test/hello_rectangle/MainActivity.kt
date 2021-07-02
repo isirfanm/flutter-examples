d60407a811ad9113db53249fc99fee9f68b5d9bc
irfan <irfan@volantis.io>
2021-07-02 21:12:32 +0700

    basic app

---
+++ android/app/src/main/kotlin/test/hello_rectangle/MainActivity.kt
@@ -0,0 +1,6 @@
+package test.hello_rectangle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/converter_route.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'unit.dart';
+
+const _padding = EdgeInsets.all(16.0);
+
+class ConverterRoute extends StatefulWidget {
+  final Color color;
+  final List<Unit> units;
+
+  const ConverterRoute({
+    @required this.color,
+    @required this.units,
+  })  : assert(color != null),
+        assert(units != null);
+
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  List<DropdownMenuItem> _unitMenuItems;
+  bool _showValidationError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _createDropdownMenuItems();
+    _setDefaults();
+  }
+
+  void _createDropdownMenuItems() {}
+
+  void _setDefaults() {}
+
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create the 'input' group of widgets. This is a Column that
+    // includes the input value, and 'from' unit [Dropdown].
+
+    // TODO: Create a compare arrows icon.
+
+    // TODO: Create the 'output' group of widgets. This is a Column that
+    // includes the output value, and 'to' unit [Dropdown].
+
+    // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
+
+    // TODO: Delete the below placeholder code.
+    final unitWidgets = widget.units.map((Unit unit) {
+      return Container(
+        color: widget.color,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subtitle1,
+            )
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
